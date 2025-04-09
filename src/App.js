0a75e509803fe397c679341472cdbded3322a337
Sengeki2 <marcos.soares@devgo.org>
2025-04-09 12:18:32 -0100

    Added corpus data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 const { containerBootstrap } = require('@nlpjs/core');
 const { Nlp } = require('@nlpjs/nlp');
 const { LangEn } = require('@nlpjs/lang-en-min');
+var obj = require('./corpus-en.json');
 
 let questions = [];
 let answers = [];
@@ -17,22 +18,7 @@ async function nlp (question) {
   const nlp = container.get('nlp');
   nlp.settings.autoSave = false;
 
-  nlp.addLanguage('en');
-  // Adds the utterances and intents for the NLP
-  nlp.addDocument('en', 'goodbye for now', 'greetings.bye');
-  nlp.addDocument('en', 'bye bye take care', 'greetings.bye');
-  nlp.addDocument('en', 'okay see you later', 'greetings.bye');
-  nlp.addDocument('en', 'bye for now', 'greetings.bye');
-  nlp.addDocument('en', 'i must go', 'greetings.bye');
-  nlp.addDocument('en', 'hello', 'greetings.hello');
-  nlp.addDocument('en', 'hi', 'greetings.hello');
-  nlp.addDocument('en', 'howdy', 'greetings.hello');
-  
-  // Train also the NLG
-  nlp.addAnswer('en', 'greetings.bye', 'Till next time');
-  nlp.addAnswer('en', 'greetings.bye', 'see you soon!');
-  nlp.addAnswer('en', 'greetings.hello', 'Hey there!');
-  nlp.addAnswer('en', 'greetings.hello', 'Greetings!');
+  await nlp.addCorpus(obj);
   await nlp.train();
   
   const response = await nlp.process('en', question);
