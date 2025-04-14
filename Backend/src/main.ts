8bbedd54143218613ac257c030e31fc3a97f2562
Sengeki1 <gamersengeki@gmail.com>
2025-04-14 00:39:11 -0100

    backend translation

---
+++ Backend/src/main.ts
@@ -0,0 +1,63 @@
+import express from 'express'
+import cors from 'cors';
+import fetch from 'node-fetch';
+import { detect } from 'langdetect';
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors()); // allow all origins
+app.use(express.json()); // parse JSON bodies
+
+app.post('/translate', async (req, res) => {
+    const textData = req.body.text;
+    const locale = req.body.locale;
+
+    console.log(req.body);
+    try {
+        const response = await fetch(`https://lingva.ml/api/v1/en/${locale}/${encodeURIComponent(textData)}`);
+        const data = await response.json() as {translation: string};
+        console.log(data);
+        res.status(200).json({ text: data.translation });
+    } catch (err) {
+        if (err.name === 'TooManyRequestsError') {
+            console.log("retry with another proxy agent");
+        }
+    }
+});
+
+app.post('/translateEN', async (req, res) => {
+    const textData = req.body.text;
+    const locale = req.body.locale;
+
+    try {
+        const response = await fetch(`https://lingva.ml/api/v1/${locale}/en/${encodeURIComponent(textData)}`);
+        const data = await response.json() as {translation: string};
+        console.log(data);
+        res.status(200).json({ text: data.translation });
+    } catch (err) {
+        if (err.name === 'TooManyRequestsError') {
+            console.log("retry with another proxy agent");
+        }
+    }
+});
+
+app.post('/guessLanguage', async (req, res) => {
+    const textData = req.body.text;
+
+    try {
+        const data = detect(textData);
+        data?.forEach((value) => {
+            if (value?.lang === 'en' || value?.lang === 'es' || value?.lang === 'pt') {
+                res.status(200).json({ text: value?.lang })
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.listen(PORT, "localhost", () => {
+    console.log("Listening on port 3001");
+});
